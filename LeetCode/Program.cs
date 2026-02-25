d7d982f5e084265f71016fb166665006c616796d
Andrew Drudge <30806777+andrewdrudge@users.noreply.github.com>
2026-02-25 13:27:18 -0600

    Project init + initial TwoSum solution + main runner setup for TwoSum solution

---
+++ LeetCode/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program {
+    public static void Main(string[] args)
+    {
+        int[] nums = [2, 7, 11, 15];
+        int target = 9;
+        TwoSum twoSum = new TwoSum();
+        int[] result = twoSum.twoSum(nums, target);
+        int[] answer = [0, 1];
+        Console.WriteLine(string.Join(",", result));
+        Console.WriteLine(string.Join(",", answer)); // sample
+        Console.WriteLine("TwoSum passed?: " + result.SequenceEqual(answer));
+    }
+}
---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,18 @@
+﻿public class TwoSum // rename to Solution if copying code to LeetCode
+{
+    public int[] twoSum(int[] nums, int target) // capitalize first letter of method name if copying code to LeetCode
+    {
+        int[] indices = [];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    indices = [i, j];
+                }
+            }
+        }
+        return indices;
+    }
+}
